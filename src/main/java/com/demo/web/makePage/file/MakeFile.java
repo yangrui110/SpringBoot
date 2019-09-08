300b36007ff58cf5df7fcffdde38aef2bff103cf
“Yourname” <Youremail@xxx.com>
2019-09-08 17:08:52 +0800

    修改代码生成器

--- src/main/java/com/demo/web/makePage/file/MakeFile.java
+++ src/main/java/com/demo/web/makePage/file/MakeFile.java
@@ -33,6 +33,13 @@ public class MakeFile {
         String pattern = FileUtil.getFileString(resource.getFile());
         System.out.println("替换前："+pattern);
 
+        ClassPathResource tableSelectResource = new ClassPathResource("htmlTemplates/inner/select-html");
+        String patternSelect = FileUtil.getFileString(tableSelectResource.getFile());
+
+        Map<String,String> mapData=new HashMap<>();
+        mapData.put("pattern", pattern);
+        mapData.put("patternSelect", patternSelect);
+
         StringBuffer result=new StringBuffer();
         //第一次遍历，获取所有需要查询的列
         Map views = new HashMap();
@@ -44,12 +51,11 @@ public class MakeFile {
         Iterator iterator = views.values().iterator();
         while (iterator.hasNext()){
             JSONObject next = (JSONObject) iterator.next();
-            StringBuffer builder=new StringBuffer();
-            String describetion = pattern.replaceAll("innerDesc-yangrui", next.getString("describetion"));
-            describetion=describetion.replaceAll("innerColumn-yangrui", next.getString("alias"));
-            builder.append(describetion);
-            System.out.println(builder.toString());
-            result.append(builder.toString());
+            String pa=pattern;
+            if(!StringUtils.isEmpty(next.get("workParentValue"))){
+                pa=patternSelect;
+            }
+            result.append(makeListPattern(next, makeListPattern(next, pa)));
         }
         return result.toString();
     }
@@ -90,17 +96,27 @@ public class MakeFile {
         ClassPathResource tableResourceReadOnly = new ClassPathResource("htmlTemplates/inner/edit-input-readOnly");
         String patternReadOnly = FileUtil.getFileString(tableResourceReadOnly.getFile());
         StringBuilder builder=new StringBuilder();
+
+        ClassPathResource tableSelectResource = new ClassPathResource("htmlTemplates/inner/select-addAndEditor-html");
+        String patternSelect = FileUtil.getFileString(tableSelectResource.getFile());
+
+        Map<String,String> map=new HashMap<>();
+        map.put("pattern", pattern);
+        map.put("patternSelect", patternSelect);
+
         Iterator iterator = columns.values().iterator();
         while (iterator.hasNext()){
             JSONObject os = (JSONObject) iterator.next();
             if(os.getBoolean("edit")){
-                String s=pattern.replaceAll("label-yangrui", os.getString("describetion"));
-                s=s.replaceAll("column-yangrui", os.getString("column"));
-                builder.append(s);
+                String pa=pattern;
+                if(!StringUtils.isEmpty(os.get("workParentValue"))){
+                    pa=patternSelect;
+                }
+                builder.append(makeListPattern(os, pa));
             }else{
-                String s=patternReadOnly.replaceAll("label-yangrui", os.getString("describetion"));
+                /*String s=patternReadOnly.replaceAll("label-yangrui", os.getString("describetion"));
                 s=s.replaceAll("column-yangrui", os.getString("column"));
-                builder.append(s);
+                builder.append(s);*/
             }
         }
         return builder.toString();
@@ -113,13 +129,23 @@ public class MakeFile {
         ClassPathResource tableResource = new ClassPathResource("htmlTemplates/inner/add-input");
         String pattern = FileUtil.getFileString(tableResource.getFile());
         StringBuilder builder=new StringBuilder();
+
+        ClassPathResource tableSelectResource = new ClassPathResource("htmlTemplates/inner/select-addAndEditor-html");
+        String patternSelect = FileUtil.getFileString(tableSelectResource.getFile());
+
+        Map<String,String> map=new HashMap<>();
+        map.put("pattern", pattern);
+        map.put("patternSelect", patternSelect);
+
         Iterator iterator = columns.values().iterator();
         while (iterator.hasNext()){
             JSONObject os = (JSONObject) iterator.next();
             if(os.getBoolean("add")){
-                String s=pattern.replaceAll("label-yangrui", os.getString("describetion"));
-                s=s.replaceAll("column-yangrui", os.getString("column"));
-                builder.append(s);
+                String pa=pattern;
+                if(!StringUtils.isEmpty(os.get("workParentValue"))){
+                    pa=patternSelect;
+                }
+                builder.append(makeListPattern(os,pa));
             }
         }
         return builder.toString();
@@ -129,21 +155,36 @@ public class MakeFile {
      * 制作增加界面的列
      * */
     public static String makeViewList(Map columns) throws IOException {
-        ClassPathResource tableResource = new ClassPathResource("htmlTemplates/inner/view-row");
+        ClassPathResource tableResource = new ClassPathResource("htmlTemplates/inner/edit-input-readOnly");
         String pattern = FileUtil.getFileString(tableResource.getFile());
         StringBuilder builder=new StringBuilder();
+
+        ClassPathResource tableSelectResource = new ClassPathResource("htmlTemplates/inner/select-view-html");
+        String patternSelect = FileUtil.getFileString(tableSelectResource.getFile());
+
+        Map<String,String> map=new HashMap<>();
+        map.put("pattern", pattern);
+        map.put("patternSelect", patternSelect);
         Iterator iterator = columns.values().iterator();
         while (iterator.hasNext()){
             JSONObject os = (JSONObject) iterator.next();
             if(os.getBoolean("view")){
-                String s=pattern.replaceAll("label-yangrui", os.getString("describetion"));
-                s=s.replaceAll("column-yangrui", os.getString("column"));
-                builder.append(s);
+                String pa=pattern;
+                if(!StringUtils.isEmpty(os.get("workParentValue"))){
+                    pa=patternSelect;
+                }
+                builder.append(makeListPattern(os, pa));
             }
         }
         return builder.toString();
     }
 
+    private static String makeListPattern(JSONObject os,String pattern){
+        String s=pattern.replaceAll("label-yangrui", os.getString("describetion"));
+        s=s.replaceAll("column-yangrui", os.getString("column"));
+        s=s.replaceAll("columnAlias-yangrui", os.getString("alias"));
+        return s;
+    }
     /**
      * 制作排序字段,以主键作为排序字段
      * */
@@ -162,26 +203,26 @@ public class MakeFile {
         ClassPathResource resource=new ClassPathResource("htmlTemplates/template");
         File[] files = resource.getFile().listFiles();
         String params = (String) condition.get("post_param");
-        String timerCompoments = registerTimerCompoments(JSONObject.parseObject(params));
+        Map<String,String> compoments = registerTimerCompoments(JSONObject.parseObject(params));
         String orderBy = getOrderByString(JSONObject.parseObject(params));
         for(File file:files){
             if("add".equals(file.getName())){
                 String addList = makeAddList(JSONObject.parseObject(params));
-                compressHtml(condition, outputStream, file,addList,timerCompoments);
+                compressHtml(condition, outputStream, file,addList,compoments);
             }else if("edit".equals(file.getName())){
                 Map<String, ColumnProperty> primaryKey = EntityMap.getPrimaryKey((String) condition.get("post_entity"));
                 String editList = makeEditList(JSONObject.parseObject(params),primaryKey);
-                compressHtml(condition, outputStream, file, editList,timerCompoments);
+                compressHtml(condition, outputStream, file, editList,compoments);
             }else if("view".equals(file.getName())){
                 String viewList = makeViewList(JSONObject.parseObject(params));
-                compressHtml(condition,outputStream ,file ,viewList,timerCompoments);
+                compressHtml(condition,outputStream ,file ,viewList,compoments);
             }else if("list".equals(file.getName())){
-                compressList(condition,outputStream,file,timerCompoments,orderBy);
+                compressList(condition,outputStream,file,compoments,orderBy);
             }
         }
     }
 
-    public static void compressList(Map condition, ZipOutputStream outputStream,File file,String timerCompoments,String orderBy) throws IOException {
+    public static void compressList(Map condition, ZipOutputStream outputStream,File file,Map<String,String> compoments,String orderBy) throws IOException {
         String entityName= (String) condition.get("post_entity");
         String params= (String) condition.get("post_param");
         String listHtml = makeListHtml(JSONObject.parseObject(params));
@@ -201,7 +242,8 @@ public class MakeFile {
                 String result = fileString.replaceAll("entityName-yangrui", mainTable.getTableAlias());//增加界面使用主表替换
                 result = result.replaceAll("aliasName-yangrui", entityName);//增加界面使用主表替换
                 result=result.replaceAll("fields-yangrui", tableList);
-                result=result.replaceAll("timerCompoments-yangrui",timerCompoments);
+                result=result.replaceAll("timerCompoments-yangrui",compoments.get("timer"));
+                result=result.replaceAll("selectCompoments-yangrui", compoments.get("select"));
                 result=result.replaceAll("orderBy-yangrui",orderBy);
                 if(StringUtils.isEmpty(orderBy)) {
                     if (DataSourceType.SQL.equals(mainTable.getInfoOfEntity().getConfig().getSourceType()))
@@ -220,7 +262,7 @@ public class MakeFile {
      * @param file 文件夹
      * @param rows 预先需要替换的行
      * */
-    public static void compressHtml(Map condition, ZipOutputStream outputStream,File file,String rows,String timerCompoments) throws IOException {
+    public static void compressHtml(Map condition, ZipOutputStream outputStream,File file,String rows,Map<String,String> compoments) throws IOException {
         String entityName= (String) condition.get("post_entity");
         MainTableInfo mainTable = EntityMap.getMainTable((String) condition.get("post_entity"));
         Map<String, ColumnProperty> primaryKey = EntityMap.getPrimaryKey(mainTable.getTableAlias());
@@ -237,7 +279,9 @@ public class MakeFile {
             }else if(f.getName().endsWith(".js")){
                 String result = fileString.replaceAll("entityName-yangrui", mainTable.getTableAlias());//增加界面使用主表替换
                 result = result.replaceAll("aliasName-yangrui", entityName);
-                result=result.replaceAll("timerCompoments-yangrui",timerCompoments);
+                result=result.replaceAll("timerCompoments-yangrui",compoments.get("timer"));
+                result=result.replaceAll("selectCompoments-yangrui", compoments.get("select"));
+                result=result.replaceAll("selectCheckedCompoments-yangrui", compoments.get("selectChecked"));
                 Compress.compressString(result, outputStream, builder.toString());
             }else if(f.getName().endsWith(".css")){
                 Compress.compressString(fileString, outputStream, builder.toString());
@@ -248,22 +292,38 @@ public class MakeFile {
     /**
      * 获取各种组件的字符串
      * */
-    private static String registerTimerCompoments(JSONObject params) throws IOException {
-        StringBuilder resultTime=new StringBuilder();
+    private static Map<String, String> registerTimerCompoments(JSONObject params) throws IOException {
+        Map<String,String> result=new HashMap<>();
+        StringBuilder timeBuilder=new StringBuilder();
+        StringBuilder workBuilder = new StringBuilder();
+        StringBuilder workCheckedBuilder = new StringBuilder();
         ClassPathResource timerResource = new ClassPathResource("htmlTemplates/inner/timer-compoments");
-        ClassPathResource workResource = new ClassPathResource("htmlTemplates/inner/timer-compoments");
-        String pattern = FileUtil.getFileString(timerResource.getFile());
+        ClassPathResource workResource = new ClassPathResource("htmlTemplates/inner/select-js");
+        ClassPathResource workCheckedResource = new ClassPathResource("htmlTemplates/inner/select-checked-js");
+        String timePattern = FileUtil.getFileString(timerResource.getFile());
+        String workPattern = FileUtil.getFileString(workResource.getFile());
+        String workCheckedPattern = FileUtil.getFileString(workCheckedResource.getFile());
         Iterator<Object> iterator = params.values().iterator();
         while (iterator.hasNext()){
             JSONObject next = (JSONObject) iterator.next();
             if("timer".equals(next.getString("compomentType"))) {
-                String patternOne = pattern.replaceAll("layDateId-yangrui", next.getString("alias"));
-                resultTime.append(patternOne);
+                String patternOne = timePattern.replaceAll("layDateId-yangrui", next.getString("alias"));
+                timeBuilder.append(patternOne);
             }else if(!StringUtils.isEmpty(next.get("workParentValue"))){
                 //存入字典
+                String parentValue = workPattern.replaceAll("dicValue-yangrui", (String) next.get("workParentValue"));
+                parentValue=parentValue.replaceAll("column-yangrui", next.getString("alias"));
+                workBuilder.append(parentValue);
+
+                String parentCheckedValue = workCheckedPattern.replaceAll("dicValue-yangrui", (String) next.get("workParentValue"));
+                parentCheckedValue=parentCheckedValue.replaceAll("column-yangrui", next.getString("alias"));
+                workCheckedBuilder.append(parentCheckedValue);
             }
         }
-        return resultTime.toString();
+        result.put("timer", timeBuilder.toString());
+        result.put("select", workBuilder.toString());
+        result.put("selectChecked", workCheckedBuilder.toString());
+        return result;
     }
 
     /**
